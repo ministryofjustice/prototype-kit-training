10b8c39928fd329fb08507eae192b79d0424b068
RholmesMOJ <55434280+RholmesMOJ@users.noreply.github.com>
2024-08-19 14:27:14 +0100

    tutorials

--- app/routes.js
+++ app/routes.js
@@ -6,4 +6,17 @@
 const govukPrototypeKit = require('govuk-prototype-kit')
 const router = govukPrototypeKit.requests.setupRouter()
 
+
 // Add your routes here
+
+
+router.post('/prototype-kit-training-page-2-submit', function (req, res) { // Make a variable and give it the value from 'how-many-balls'
+var radiobuttonname = req.session.data['choicenewname']
+// Check whether the variable matches a condition
+if (radiobuttonname == "Yes"){
+// Send user to next page
+res.redirect('prototype-kit-training-page-3')}
+else {
+// Send user to ineligible / check answers page
+res.redirect('prototype-kit-training-page-4')
+} })
